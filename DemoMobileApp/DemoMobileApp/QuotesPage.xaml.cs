a489f41d7ea567164b331eb3ecf8d95f5a51ac31
Arockia Magimai Nathan J <arockiaraj@pdinet.com>
2018-12-17 12:25:09 +0530

    Add project files.

---
+++ DemoMobileApp/DemoMobileApp/QuotesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        string[] quotes = { "Quote1", "Quote2", "Quote3", "Quote4", "Quote5", "Quote6" };
+        int current = 1;
+        int max = 7;
+        public QuotesPage()
+        {
+            InitializeComponent();
+
+            lblQuote.Text = quotes[current - 1];
+        }
+
+        private void btnNext_Clicked(object sender, EventArgs e)
+        {
+            if (current == max)
+                current = 1;
+            lblQuote.Text = quotes[current - 1];
+            current++;
+        }
+    }
+}
---
+++ DemoMobileApp/DemoMobileApp/RelativePageExOne.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoMobileApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RelativePageExOne : ContentPage
+	{
+		public RelativePageExOne ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
